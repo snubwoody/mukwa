5137891bab83c28c3be12053c9e10ec55e4e8911
Wakunguma K. <contact@wakunguma.com>
2026-07-16 00:57:08 -0600

    Add lucide icons

--- build.rs
+++ build.rs
@@ -1,3 +1,11 @@
+use std::{collections::HashMap, path::PathBuf};
+
 fn main() {
-    slint_build::compile("ui/app.slint").unwrap();
+    let library = HashMap::from([(
+        "lucide".to_string(),
+        PathBuf::from(lucide_slint::lib()),
+    )]);
+    let config = slint_build::CompilerConfiguration::new()
+        .with_library_paths(library);
+    slint_build::compile_with_config("ui/app.slint",config).unwrap();
 }
