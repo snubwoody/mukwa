dc63ece9b7c8026da91bd0f5aa2ce0c27e2aa121
Wakunguma K. <contact@wakunguma.com>
2026-07-16 17:09:14 -0600

    Position accounts under 'All Accounts' button

--- build.rs
+++ build.rs
@@ -1,11 +1,7 @@
 use std::{collections::HashMap, path::PathBuf};
 
 fn main() {
-    let library = HashMap::from([(
-        "lucide".to_string(),
-        PathBuf::from(lucide_slint::lib()),
-    )]);
-    let config = slint_build::CompilerConfiguration::new()
-        .with_library_paths(library);
-    slint_build::compile_with_config("ui/app.slint",config).unwrap();
+    let library = HashMap::from([("lucide".to_string(), PathBuf::from(lucide_slint::lib()))]);
+    let config = slint_build::CompilerConfiguration::new().with_library_paths(library);
+    slint_build::compile_with_config("ui/app.slint", config).unwrap();
 }
