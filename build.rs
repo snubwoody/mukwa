3051d2d552486768fa3935e06c88cbc4f64d559c
Wakunguma K. <contact@wakunguma.com>
2026-07-21 18:33:24 -0600

    Embed windows resource data

--- build.rs
+++ build.rs
@@ -4,4 +4,16 @@ fn main() {
     let library = HashMap::from([("lucide".to_string(), PathBuf::from(lucide_slint::lib()))]);
     let config = slint_build::CompilerConfiguration::new().with_library_paths(library);
     slint_build::compile_with_config("ui/app.slint", config).unwrap();
+
+    #[cfg(target_os = "windows")]
+    {
+        use winresource::WindowsResource;
+
+        let mut rc = WindowsResource::new();
+        rc.set("ProductName", "Mukwa");
+        rc.set("FileDescription", "Mukwa");
+        rc.set("LegalCopyright", "Copyright © 2026 Wakunguma Kalimuwka");
+        rc.set("CompanyName", "Wakunguma Kalimuwka");
+        rc.compile().unwrap()
+    }
 }
