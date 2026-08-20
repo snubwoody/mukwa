a88e10824fb720891743cfd785382b9b0efafa8b
Wakunguma Kalimukwa <contact@wakunguma.com>
2026-08-20 15:34:21 -0600

    Fix license header

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 ofuse slint :: private_unstable_api :: re_exports as sp;
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
