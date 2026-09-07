29315b28787e7b65cc28427cf2b13f6333bc11d9
Wakunguma Kalimukwa <contact@wakunguma.com>
2026-09-07 11:48:22 -0600

    Run cargo fmt

--- tests/build.rs
+++ tests/build.rs
@@ -1,28 +1,30 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2026 Wakunguma Kalimukwa
 
+use i_slint_compiler::generator::OutputFormat;
+use i_slint_compiler::{
+    CompilerConfiguration, compile_syntax_node, diagnostics::BuildDiagnostics, generator, parser,
+};
 use std::collections::HashMap;
-use std::io::Write;
 use std::fs;
 use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
-use i_slint_compiler::{diagnostics::BuildDiagnostics, parser, CompilerConfiguration, compile_syntax_node, generator};
-use i_slint_compiler::{generator::OutputFormat};
 
-fn extract_test_function(source: &str) -> String{
+fn extract_test_function(source: &str) -> String {
     let mut in_comment = false;
     let mut function = String::new();
-    for line in source.lines(){
-        if line == "/*"{
+    for line in source.lines() {
+        if line == "/*" {
             in_comment = true;
             continue;
         }
 
-        if line == "*/"{
+        if line == "*/" {
             break;
         }
 
-        if !in_comment{
+        if !in_comment {
             continue;
         }
 
@@ -45,51 +47,64 @@ fn main() {
     let cases_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("cases");
 
     let mut test_cases = vec![];
-    for entry in fs::read_dir(&cases_dir).unwrap(){
+    for entry in fs::read_dir(&cases_dir).unwrap() {
         let entry = entry.unwrap();
         // TODO: add recursive function for dirs
 
         let file_name = entry.file_name();
         let name: Vec<&str> = file_name.to_str().unwrap().split(".").collect();
         let source = fs::read_to_string(entry.path()).unwrap();
-        let case = TestCase{
+        let case = TestCase {
             source,
-            name: String::from(name[0])
+            name: String::from(name[0]),
         };
         test_cases.push(case);
     }
 
     let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-    let include_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).parent().unwrap().join("crates/mukwa/ui");
+    let include_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
+        .join("crates/mukwa/ui");
     dbg!(&include_path);
 
-    for case in test_cases{
+    for case in test_cases {
         let mut diag = BuildDiagnostics::default();
 
-        let syntax_node = parser::parse(case.source.clone(),None,&mut diag);
+        let syntax_node = parser::parse(case.source.clone(), None, &mut diag);
         let library = HashMap::from([("lucide".to_string(), PathBuf::from(lucide_slint::lib()))]);
         let mut compiler_config = CompilerConfiguration::new(OutputFormat::Rust);
         compiler_config.debug_info = true;
         compiler_config.include_paths = vec![include_path.clone()];
         compiler_config.library_paths = library;
 
+        let (root_component, diag, loader) =
+            smol::block_on(compile_syntax_node(syntax_node, diag, compiler_config));
 
-        let (root_component,diag,loader) = smol::block_on(compile_syntax_node(syntax_node,diag,compiler_config));
-
-
-        if diag.has_errors(){
+        if diag.has_errors() {
             diag.print_warnings_and_exit_on_error();
             // TODO: maybe return an error here
         } else {
             diag.print();
         }
 
-        let mut file = File::create(out_dir.join(format!("{}.rs",&case.name))).unwrap();
-        generator::generate(OutputFormat::Rust,&mut file,None,&root_component,&loader.compiler_config).unwrap();
+        let mut file = File::create(out_dir.join(format!("{}.rs", &case.name))).unwrap();
+        generator::generate(
+            OutputFormat::Rust,
+            &mut file,
+            None,
+            &root_component,
+            &loader.compiler_config,
+        )
+        .unwrap();
 
         let test_function = extract_test_function(&case.source);
 
-        write!(file,"\n#[test]\nfn test_{}(){{\ni_slint_backend_testing::init_no_event_loop();\n{}}}",case.name,test_function).unwrap();
+        write!(
+            file,
+            "\n#[test]\nfn test_{}(){{\ni_slint_backend_testing::init_no_event_loop();\n{}}}",
+            case.name, test_function
+        )
+        .unwrap();
     }
 }
-
--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2026 Wakunguma Kalimukwa
 
-fn main() {
-}
+fn main() {}
