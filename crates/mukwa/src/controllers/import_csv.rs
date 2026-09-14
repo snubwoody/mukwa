02094cf5eef7b726b5839bda5da91913ea9de37d
Wakunguma Kalimukwa <contact@wakunguma.com>
2026-09-13 22:12:28 -0600

    Move import state into ImportCsvState

---
+++ crates/mukwa/src/controllers/import_csv.rs
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2026 Wakunguma Kalimukwa
+
+use slint::ComponentHandle;
+use crate::ui;
+use crate::ui::MainWindow;
+
+pub fn bind(window: &MainWindow){
+    let state = window.global::<ui::ImportCsvState>();
+}
+
